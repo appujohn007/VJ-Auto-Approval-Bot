ead67efce92f12d53632f46cbda12ed706de9559
Appu John <161853537+appujohn007@users.noreply.github.com>
2024-07-11 14:42:33 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -3,10 +3,10 @@ from os import path, getenv
 class Config:
     API_ID = int(getenv("API_ID", "10471716"))
     API_HASH = getenv("API_HASH", "f8a1b21a13af154596e2ff5bed164860")
-    BOT_TOKEN = getenv("BOT_TOKEN", "6916875347:AAEVxR4cO_sIBB6V57ANA92pHKxzw9G3yX0")
+    BOT_TOKEN = getenv("BOT_TOKEN", "7240311570:AAEzQj2J_sQq7cIPyuGZxIMHpnTANYd37GM")
     FSUB = getenv("FSUB", "botio_devs")
-    CHID = int(getenv("CHID", "-1002054575318"))
-    SUDO = list(map(int, getenv("SUDO", "6883997969").split()))
+    CHID = int(getenv("CHID", "-1001778617703"))
+    SUDO = list(map(int, getenv("SUDO", "6883997969 5460214165").split()))
     MONGO_URI = getenv("MONGO_URI", "mongodb+srv://appuz:chrijismiappuz@cluster0.yngvhc2.mongodb.net/?retryWrites=true&w=majority")
     
 cfg = Config()
